f706a9aa1277be6a212eaaff8ee57d12f0e64845
Cameron <cameron.morgan@skiff.com>
2023-08-27 09:26:26 -0400

    Added functionality to the 'logger' middleware.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,10 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const reqMethod = req.method
+  const reqUrl = req.originalUrl
+  const reqTimestamp = new Date().toLocaleString()
+  console.log(`${reqMethod}, ${reqUrl}, ${reqTimestamp}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +20,6 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+}
